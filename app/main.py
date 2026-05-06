31b671827e495edb192316b5af9bfaf20f016a74
Mirash1l1 <killer28392@gmail.com>
2026-05-06 20:12:08 +0300

    Сделано задание

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for dict_people in people:
+
+        result.append(Person(dict_people["name"], dict_people["age"]))
+
+    for dict_people in people:
+
+        person = Person.people[dict_people["name"]]
+
+        if dict_people.get("wife"):
+            person.wife = Person.people[dict_people["wife"]]
+
+        if dict_people.get("husband"):
+            person.husband = Person.people[dict_people["husband"]]
+
+    return result
